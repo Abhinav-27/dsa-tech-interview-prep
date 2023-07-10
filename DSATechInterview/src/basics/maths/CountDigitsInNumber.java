e1df9326a6b38b8b7e330040b9c526372bc6d4d6
Abhinav Singh <msabhinavsingh@gmail.com>
2023-07-11 00:41:46 +0530

    Enhanced programs.

--- DSATechInterview/src/basics/maths/CountDigitsInNumber.java
+++ DSATechInterview/src/basics/maths/CountDigitsInNumber.java
@@ -8,15 +8,28 @@ public class CountDigitsInNumber {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bufferedReader.readLine());
-        System.out.println(countDigits(N));
+        System.out.println(countDigitsUsingCounter(N));
+        System.out.println(countDigitsUsingLog(N));
+        System.out.println(countDigitsUsingStringConversion(N));
     }
 
-    private static int countDigits(int n) {
+    // Using Divide & counter approach but Time Complexity is O(n).
+    private static int countDigitsUsingCounter(int n) {
         int count = 0;
         while (n != 0){
             n = n / 10;
             count++;
         }
         return count;
+    }
+
+    // Using Logarithmic approach where Time Complexity is O(1).
+    private static int countDigitsUsingLog(int n) {
+        return (int) Math.floor(Math.log10(n) + 1);
+    }
+
+    // Using Integer to String conversion approach where Time Complexity is O(1).
+    private static int countDigitsUsingStringConversion(int n) {
+        return Integer.toString(n).length();
     }
 }
--- DSATechInterview/src/basics/maths/GCDRecursion.java
+++ DSATechInterview/src/basics/maths/GCDRecursion.java
@@ -13,7 +13,7 @@ public class GCDRecursion {
     }
 
     private static int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a%b);
+        if (a == 0) return b;
+        return gcd(b%a, a);
     }
 }
